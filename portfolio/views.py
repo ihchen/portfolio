c66f7bedd6787effbea5e95b934699b0d5231372
Imanuel Chen <imanuelhchen@gmail.com>
2017-04-23 19:59:06 -0700

    Prevent browser caching

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,6 @@ from flask import render_template
 
 @app.route('/')
 def portfolio():
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
     return render_template('index.html')
