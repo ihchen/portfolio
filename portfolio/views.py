a98bfbadd871cf556477fbdbedf1c70ef3d3cbaf
Imanuel Chen <imanuelhchen@gmail.com>
2017-04-23 20:04:36 -0700

    Removed added headers to prevent browser caching

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,4 @@ from flask import render_template
 
 @app.route('/')
 def portfolio():
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
     return render_template('index.html')
