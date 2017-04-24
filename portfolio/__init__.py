de88eec1fb7b36c00213f766c82779a8a401a092
Imanuel Chen <imanuelhchen@gmail.com>
2017-04-23 19:43:44 -0700

    Changed configuration naming

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('default_settings')
+app.config.from_object('portfolio_config')
 app.config.from_envvar('PORTFOLIO_SETTINGS', silent=True)
 
 from portfolio import views
