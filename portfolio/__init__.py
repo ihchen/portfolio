c8d16bb3995bbd6cd0798582e51504f2121a78ed
Imanuel Chen <imanuelhchen@gmail.com>
2017-04-25 00:20:48 -0700

    Put views in init since very little views

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('portfolio_config')
 app.config.from_envvar('PORTFOLIO_SETTINGS', silent=True)
 
-from portfolio import views
+@app.route('/')
+def portfolio():
+    return render_template('index.html')
--- portfolio/views.py
+++
@@ -1,6 +0,0 @@
-from portfolio import app
-from flask import render_template
-
-@app.route('/')
-def portfolio():
-    return render_template('index.html')
